8d8afc96a2a1a950a54b0750c81d5597d026de2e
MartienJun <martienjunaedi123@gmail.com>
2021-06-13 16:20:54 +0700

    feat: add flask-login login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
-from app.Views.dashboard import dashboard
-from flask import Flask, Blueprint, session, render_template
-from app.Views import dashboard, role_view, user_view, profile_view, transportation_view, post_transport_view, post_view, comment_view
+from flask import Flask, Blueprint, session, render_template, redirect, url_for
+from flask_login import LoginManager
+from app.Views import auth, dashboard, role_view, user_view, profile_view, transportation_view, post_transport_view, post_view, comment_view
+from app.Controllers.user_controller import UserController
 from app.my_database import MyDatabase
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'travelnesia'
 
+# Set database
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
@@ -14,7 +16,8 @@ app.config['MYSQL_DATABASE_DB'] = 'travelnesia'
 MyDatabase.mysql.init_app(app)
 
 
-# Admin Views
+# Set blueprint
+app.register_blueprint(auth.blueprint)
 app.register_blueprint(dashboard.blueprint)
 app.register_blueprint(role_view.blueprint)
 app.register_blueprint(user_view.blueprint)
@@ -24,7 +27,17 @@ app.register_blueprint(post_transport_view.blueprint)
 app.register_blueprint(post_view.blueprint)
 app.register_blueprint(comment_view.blueprint)
 
+
+ #Login manager settings
+login_manager = LoginManager()
+login_manager.login_view = 'auth.signin'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return UserController.get_by_id(id)
+
+
 @app.route('/')
 def index():
-    session['admin'] = 'admin'
     return render_template("index.html")
