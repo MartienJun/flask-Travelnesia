914611bcedf79c3c7892a61388b5959fb94f4a2f
MartienJun <martienjunaedi123@gmail.com>
2021-06-13 23:49:50 +0700

    refactor: profile view and update

--- app/Views/profile_view.py
+++ app/Views/profile_view.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
-from app.Controllers.profile_controller import ProfileController
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask.helpers import flash
+from flask_login import current_user, login_required
+from app.Models.user import User
 from app.Models.profile import Profile
+from app.Controllers.profile_controller import ProfileController
+from app.Controllers.role_controller import RoleController
 from app.Controllers.user_controller import UserController
 
 
@@ -11,78 +15,47 @@ blueprint = Blueprint("profile", __name__, url_prefix="/admin/profile")
 # Routing untuk ke halaman view
 @blueprint.route('/')
 @blueprint.route('/view')
+@login_required
 def view():
-    # Jika session admin tidak ada, redirect kembali ke home
-    if session.get('admin') is None:
-        return redirect(url_for('home'))
+    # Jika session rolenya bukan admin, redirect kembali ke sign in
+    if current_user.role != 'adm':
+        flash("You must sign in as admin to use this feature")
+        return redirect(url_for('auth.signin'))
     # Jika session admin ada, tampilkan halaman view
-    return render_template("Views/profile/view.html", list_profile=ProfileController.get_all(), list_user=UserController.get_all())
-
-
-# Routing untuk halaman insert
-@blueprint.route('/insert', methods=['GET', 'POST'])
-def insert():
-    # Jika session admin tidak ada, redirect kembali ke home
-    if session.get('admin') is None:
-        return redirect(url_for('home'))
-    # Jika metodenya adalah get, tampilkan halaman insert
-    if request.method == 'GET':
-        return render_template("Views/profile/insert.html", list_user=UserController.get_all())
-
-    # Jika metodenya adalah post, dapatkan data dari post
-    username = request.form['username']
-    name = request.form['name']
-    email = request.form['email']
-    telp = request.form['telp']
-    profile_pict = request.form['profile_pict']
-
-    # Cek apakah username sudah ada dalam database
-    if ProfileController.get_by_id(username) is not None:
-        # jika iya, tampilkan error message
-        return render_template('Views/profile/insert.html', message="username sudah pernah terdaftar!", list_user=UserController.get_all())
-
-    # Jika data sudah sesuai, masukan data tersebut ke dalam database melalui model
-    profile = Profile(username, name, email, telp, profile_pict)
-    ProfileController.insert(profile)
-
-    # Redirect ke halaman view
-    return redirect(url_for('profile.view'))
+    return render_template("admin/profile/view.html", profile=ProfileController.get_by_id(current_user.username), list_role=RoleController.get_all(), user=UserController.get_by_id(current_user.username))
 
 
 # Routing untuk halaman update
 @blueprint.route('/update/<id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
-    # Jika session admin tidak ada, redirect kembali ke home
-    if session.get('admin') is None:
-        return redirect(url_for('home'))
+    # Jika session rolenya bukan admin, redirect kembali ke sign in
+    if current_user.role != 'adm':
+        flash("You must sign in as admin to use this feature")
+        return redirect(url_for('auth.signin'))
     # Jika metodenya adalah get, tampilkan halaman update
     if request.method == 'GET':
-        return render_template("Views/profile/update.html", profile=ProfileController.get_by_id(id), list_user=UserController.get_all())
+        return render_template("admin/profile/update.html", profile=ProfileController.get_by_id(id), list_role=RoleController.get_all(), user=UserController.get_by_id(id))
 
     # Jika metodenya adalah post, dapatkan data dari post
     username = request.form['username']
     name = request.form['name']
     email = request.form['email']
     telp = request.form['telp']
-    profile_pict = request.form['profile_pict']
+    role = request.form['role']
+    password = request.form['password']
+    profile_pict = request.files['profile_pict']
+    p_pict = profile_pict.filename
 
+    # Jika profile picture tidak diisi
+    if p_pict == "":
+        p_pict = ProfileController.get_by_id(id).profile_pict
+    
     # Update data tersebut ke dalam database melalui model
-    profile = Profile(username, name, email, telp, profile_pict)
+    user = User(username, role, password)
+    profile = Profile(username, name, email, telp, p_pict)
+    UserController.update(user)
     ProfileController.update(profile)
 
     # Redirect kembali ke view
-    return redirect(url_for('profile.view'))
-
-
-# Routing untuk halaman delete
-@blueprint.route('/delete/<id>', methods=['POST'])
-def delete(id):
-    # Jika session admin tidak ada, redirect kembali ke home
-    if session.get('admin') is None:
-        return redirect(url_for('home'))
-
-    # Hapus data tersebut dari database
-    ProfileController.delete(id)
-
-    # Redirect kembali ke View
     return redirect(url_for('profile.view'))
