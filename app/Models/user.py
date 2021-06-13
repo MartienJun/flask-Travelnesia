3dea9d8b63ea8ca9b247c76e060eccadbcfbe616
MartienJun <martienjunaedi123@gmail.com>
2021-06-13 16:30:52 +0700

    fix: add UserMixin and get_id()
    UserMixin for
    Flask-login AttributeError: 'User' object has no attribute 'is_active'
    
    get_id() for
    NotImplementedError: No `id` attribute - override `get_id`

--- app/Models/user.py
+++ app/Models/user.py
@@ -1,7 +1,11 @@
+from flask_login import UserMixin
 # Class ini digunakan untuk menyimpan data User dari database travelnesia
-class User:
+class User(UserMixin):
     # Constructor ini digunakan untuk inisialiasi data dari class User
     def __init__(self, username, role, password):
         self.username = username
         self.role = role
         self.password = password
+    
+    def get_id(self):
+           return (self.username)
