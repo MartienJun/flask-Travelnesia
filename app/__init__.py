de65de01a37bdbea6da9c62ab2f2537baea4be33
MartienJun <martienjunaedi123@gmail.com>
2021-06-17 17:31:22 +0700

    feat: user redirect when already signin

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from flask_login import LoginManager
+from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, current_user
+from flask_login.utils import login_user
 from app.Views import auth, role_view, user_view, profile_view, transportation_view, post_view, comment_view
 from app.Views.UserViews import user_post_view, user_comment_view, user_profile_view
 from app.Controllers.user_controller import UserController
@@ -45,4 +46,10 @@ def load_user(id):
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated is True:
+        if current_user.role == 'adm':
+            return redirect(url_for('post.view'))
+        
+        return redirect(url_for('user_post.view'))
+
     return render_template("index.html")
